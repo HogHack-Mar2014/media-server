258f6c41280bd384ec13bcec05e1f93f41df5c54
Tomasz Maćkowiak <kurazu@kurazu.net>
2014-03-13 14:46:49 +0100

    made /API/version return JSON

--- app.js
+++ app.js
@@ -9,7 +9,64 @@ var
   server = http.createServer(app);
 
 app.get('/API/version', function (req, res) {
-    res.send('hello world');
+    var version_info = {
+        "component" : [
+            {
+                "version" : "3.3.10.pre-g0d99a67-11431",
+                "name" : "API"
+            }
+        ],
+        "licenseInfo" : {
+            "licenseStatus" : "valid",
+            "itemNumber" : {
+                "allowed" : "-1",
+                "current" : "10123"
+            },
+            "transcoderNumber" : {
+                "allowed" : "-1",
+                "current" : "1"
+            },
+            "userNumber" : {
+                "allowed" : "-1",
+                "current" : "2289"
+            },
+            "resellerCompanyName" : "Hogarth Worldwide",
+            "endCustomerCompanyname" : "Hogarth Worldwide",
+            "licenseType" : "2",
+            "resellerCompanyContactEmail" : "softwarelicensing@hogarthww.com",
+            "expiryDate" : "2014-06-30",
+            "codecStatus" : {
+                "codec" : [
+
+                ]
+            },
+            "macaddresses" : {
+                "macaddress" : [
+                    "00:0c:29:4b:09:9e",
+                    "00:0c:29:5e:cc:02",
+                    "00:0c:29:99:f8:87",
+                    "00:0c:29:ae:d0:89",
+                    "00:0c:29:b2:6e:44",
+                    "00:0c:29:d5:7a:8d",
+                    "00:0c:29:dc:08:a7",
+                    "00:0c:29:df:e2:c9",
+                    "00:50:56:aa:44:fc",
+                    "00:50:56:aa:6e:ab"
+                ]
+            },
+            "storageNumber" : {
+                "allowed" : "-1",
+                "current" : "1"
+            },
+            "endCustomerCompanyContactEmail" : "softwarelicensing@hogarthww.com"
+        },
+        "systemInfo" : {
+            "macaddress" : [
+                "00:0c:29:d5:7a:8d"
+            ]
+        }
+    };
+    res.json(version_info);
 });
 
 server.listen(3000);
